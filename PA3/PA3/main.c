3b21f2d1f3fd0acbde21040abcf0b3356d8838dc
Ben <swanzeyb2001@gmail.com>
2022-09-19 14:50:38 -0700

    Read values from input file

--- PA3/PA3/main.c
+++ PA3/PA3/main.c
@@ -11,15 +11,51 @@
 
 int main() {
 	// Open the student records file
-	FILE* infile = fopen("input.dat", "r");
+	FILE* records = fopen("input.dat", "r");
 	// Open the out file for saving results
-	FILE* outfile = fopen("output.dat", "w");
+	FILE* results = fopen("output.dat", "w");
 
-	
+	// Use arrays to create a dictionary-like lookup for students
+	double GPAs[5] = { 0.0 }; // Students GPAs
+	int levels[5] = { 0 }; // Students Class Standings
+	double ages[5] = { 0.0 }; // Students Ages
+
+	// We know that the file only has 5 student entries, so lets iterate over them.
+	// Oh side note, we could try counting the amount of blank lines to
+	// figure out how many entries are in the file.
+	for (int i = 0; i < 5; i += 1) {
+		// We don't use the student Id for anything in this progarm, so it's not being assigned here
+		read_integer(records); // Coordinates to the student Id in the stream
+		GPAs[i] = read_double(records); // The current student's GPA
+		levels[i] = read_integer(records); // The current student's class standing
+		ages[i] = read_double(records); // The current student's age
+	}
+
+	// Calculate the sum of GPAs
+
+	// Calculate the sum of the class standings
+
+	// Calculate the sum of ages
+
+	// Calculate the mean of GPAs and save the result
+
+	// Calculate the mean of class standing and save the result
+
+	// Calculate the mean of ages and save the result
+
+	// Calculate the deviation of each GPA from the mean
+
+	// Calculate the variance of GPAs
+
+	// Calculate the standard deviation of the GPAs and save the result
+
+	// Find the min of the GPAs and save the result
+
+	// Find the max of the GPAs and save the result
 
 	// Close the files because we're done with them
-	fclose(infile);
-	fclose(outfile);
+	fclose(records);
+	fclose(results);
 
 	// Tell the system the program executed successfully
 	return 0;
