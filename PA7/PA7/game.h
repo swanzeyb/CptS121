63f2741e4e420611a6fce5898e1b43eb79bc9c4c
Ben <swanzeyb2001@gmail.com>
2022-11-28 08:20:12 -0800

    Start PA7

---
+++ PA7/PA7/game.h
@@ -0,0 +1,127 @@
+/*******************************************************************************************
+* Programmer: Benjamin Swanzey <benjamin.swanzey@wsu.edu>
+* Class: CptS 121, Fall 2022; Lab Section 1
+* Programming Assignment: PA6
+* Date: November 2nd, 2022
+* Description: This module describes the games logic and rules.
+*******************************************************************************************/
+
+// Header guard to ensure that our code is only included once by the compiler
+#ifndef GAME_H
+
+// Define contstants
+#define GAME_H
+#define _CRT_SECURE_NO_WARNINGS // To ignore scanf warning
+
+// The required libraries
+#include <stdio.h> // Include our standard functions for interacting with the IO stream
+#include <stdlib.h> // Gives us access to system function
+#include <time.h> // This gives access to time func used in srand
+#include <ctype.h>
+#include <conio.h>
+
+// The Carrier has 5 cells, Battleship has 4 cells, Cruiser has 3 cells, Submarine has 3 cells, and the Destroyer has 2 cells.
+enum Ships {
+  NoShip = 0,
+  Destroyer = 1,
+  Submarine = 2,
+  Cruiser = 3,
+  Battleship = 4,
+  Carrier = 5,
+};
+
+// If this was C++, I think I would use polymorphism to extend a base ship class
+// and then make all the different ship types.
+
+typedef struct {
+  char display;
+  int length;
+  enum Ships type;
+  int x_lower;
+  int y_lower;
+  int x_upper;
+  int y_upper;
+} Ship;
+
+typedef struct {
+  Ship* who_is[10][10];
+  char display[10][10];
+  int misses[10][10];
+  int hits[10][10];
+} Board;
+
+typedef struct {
+  int x;
+  int y;
+} Coord;
+
+typedef struct {
+  int total_hits;
+  int total_misses;
+  int total_shots;
+  double hits_to_misses;
+  int is_winner;
+} Stats;
+
+typedef struct {
+  Board* p1_board;
+  Board* p2_board;
+  Ship* p1_fleet;
+  Ship* p2_fleet;
+  Stats* p1_stats;
+  Stats* p2_stats;
+  int is_setup;
+  int rounds;
+  int curr_place;
+  Coord* cursor;
+} State;
+
+// I want to support a bunch of different colors,
+// and I found out that C supports enums so ima
+// try them out.
+typedef enum {
+  RED = 31,
+  GREEN = 32,
+  YELLOW = 33,
+  BLUE = 34,
+  MAGENTA = 35,
+  CYAN = 36,
+  GRAY = 37,
+  ALT_GRAY = 90,
+  ALT_RED = 91,
+  ALT_GREEN = 92,
+  ALT_YELLOW = 93,
+  ALT_BLUE = 94,
+  ALT_MAGENTA = 95,
+  ALT_CYAN = 96,
+  WHITE = 97,
+} Colors;
+
+// -- Helper functions --
+void clear_terminal();
+int get_menu_key();
+char wait_for_input();
+int sum_array(int nums[], int length);
+int is_zero_default(int val_one, int val_two);
+void color(Colors color);
+void reset();
+int rand_num(int len);
+
+// -- The functions that purely print info to console --
+void display_main_menu();
+void display_rules();
+
+// -- The components that make up scenes --
+int roll_die(void);
+
+// -- The games main scenes --
+void goto_scene(int (*scene)(char, State*), State* state);
+int rules_scene(char input, State* state);
+int game_scene(char input, State* state);
+
+// -- Game Stuff
+void init_fleet(Ship fleet[]);
+void init_board(Board *board);
+// void display_board(Board *board);
+
+#endif
---
+++ PA7/PA7/main.c
@@ -0,0 +1,69 @@
+/*******************************************************************************************
+* Programmer: Benjamin Swanzey <benjamin.swanzey@wsu.edu>
+* Class: CptS 121, Fall 2022; Lab Section 1
+* Programming Assignment: PA5
+* Date: October 7th, 2022
+* Description: Write a program that implements Battleship.
+*******************************************************************************************/
+
+#include "game.h"
+
+int main() {
+	srand(time(NULL));
+	// system("/bin/stty cooked");
+	
+	int continue_game = 1;
+	int current_menu = -1;
+
+	Board p1_board;
+	Board p2_board;
+
+	init_board(&p1_board);
+	init_board(&p2_board);
+
+	Ship p1_fleet[5];
+	Ship p2_fleet[5];
+
+	init_fleet(p1_fleet);
+	init_fleet(p2_fleet);
+
+	Stats p1_stats = { 0 };
+	Stats p2_stats = { 0 };
+
+	Coord cursor = { -1, -1 };
+
+	State state = {
+		&p1_board,
+		&p2_board,
+		p1_fleet,
+		p2_fleet,
+		&p1_stats,
+		&p2_stats,
+		0, // is_setup
+		0, // rounds
+		0, // curr_place
+		&cursor,
+	};
+
+	// Now wait for user's input
+	while (continue_game == 1) {
+		clear_terminal();
+		display_main_menu();
+		current_menu = get_menu_key();
+
+		switch(current_menu) {
+			case 1:
+				goto_scene(game_scene, &state);
+				break;
+			case 2:
+				goto_scene(rules_scene, &state);
+				break;
+			case 3:
+				continue_game = 0;
+				break;
+		}
+	}
+
+	// Tell the system the program executed successfully
+	return 0;
+}
